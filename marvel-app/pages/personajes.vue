c013d105f977ae5d834183482748065db120aa1f
PaulinaMS2 <paulinasaldarriaga34@gmail.com>
2023-09-26 15:33:58 -0500

    Feat: Dandole estilos a cards de los personajes.

--- marvel-app/pages/personajes.vue
+++ marvel-app/pages/personajes.vue
@@ -1,16 +1,19 @@
 <template >
     <v-container class="container">
+      <h1 class="titulo">Personajes de Marvel</h1> 
+      <br>
       <v-row>
         <v-col v-for="personaje in personajes" :key="personaje.id" cols="12"
           sm="6"
           md="4"
           lg="3">
-          <v-card class="personaje-card" >
+          <v-card class="personaje-card" elevation="8"   >
+          <v-card-title class="nombreP">{{ personaje.name }}</v-card-title>
           <v-img class="personaje-img" :src="personaje.thumbnail.path + '.' + personaje.thumbnail.extension"></v-img>
-          <v-card-title>{{ personaje.name }}</v-card-title>
+          
           <v-card-actions>
               <v-btn class="btn" variant="outlined" @click="viewMore()">
-                  Detalles 
+                  Ver personaje
               </v-btn>
           </v-card-actions>
           </v-card>
@@ -21,17 +24,38 @@
   </template>
   
   <style>
+
+  .btn{
+    border-radius: 40px !important;
+    background-color: black !important;
+    color: white !important;
+    border-color: white !important;
+  }
   
+  .nombreP{
+    font-family:sans-serif;
+    font-weight: bold !important;
+    text-align: center;
+  }
+
+  .titulo{
+    font-family:sans-serif;
+    font-weight: bold;
+    text-align: center;
+  }
+
   .container {
     align-items: justify !important;
     justify-content: justify !important;
     flex-direction: column !important;
+    overflow: auto !important;
    }
   
   .personaje-card {
       background-color: whitesmoke;
-      border-color: black;
+      border: 1px solid #5868a4 !important; 
       color: black;
+      border-radius: 15px !important;
       
   }
   
